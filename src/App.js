9864edae07cbc9d57b8fd488132f32efa18a9192
egoing <egoing@gmail.com>
2020-02-11 13:28:48 +0900

    3.1. class 스타일에서 state 사용법

--- src/App.js
+++ src/App.js
@@ -5,25 +5,35 @@ function App() {
   return (
     <div className="container">
       <h1>Hello World</h1>
-      <FuncComp></FuncComp>
-      <ClassComp></ClassComp>
+      <FuncComp initNumber={2}></FuncComp>
+      <ClassComp initNumber={2}></ClassComp>
     </div> 
   );
 }
 
-function FuncComp(){
+function FuncComp(props){
   return (
     <div className="container">
       <h2>function style component</h2>
+      <p>Number : {props.initNumber}</p>
     </div>
   );
 }
 
 class ClassComp extends React.Component{
+  state = {
+    number:this.props.initNumber
+  }
   render(){
     return (
       <div className="container">
         <h2>class style component</h2>
+        <p>Number : {this.state.number}</p>
+        <input type="button" value="random" onClick={
+          function(){
+            this.setState({number:Math.random()})
+          }.bind(this)
+        }></input>
       </div>
     )
   }
