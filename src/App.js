7e2101c7b47602ec96f0bb98a705e358fc07aff8
egoing <egoing@gmail.com>
2020-02-11 13:27:53 +0900

    2. 수업의 목표

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello World</h1>
+      <FuncComp></FuncComp>
+      <ClassComp></ClassComp>
+    </div> 
+  );
+}
+
+function FuncComp(){
+  return (
+    <div className="container">
+      <h2>function style component</h2>
     </div>
   );
+}
+
+class ClassComp extends React.Component{
+  render(){
+    return (
+      <div className="container">
+        <h2>class style component</h2>
+      </div>
+    )
+  }
 }
 
 export default App;
