608a67dfc0259f3f8d6f8828e3417f6e52327513
egoing <egoing@gmail.com>
2020-02-12 01:13:59 +0900

    4.2.4. 함수에서 라이프 사이클 구현하기 - skipping effect

--- src/App.js
+++ src/App.js
@@ -23,14 +23,30 @@ function FuncComp(props){
 
   var [_date, setDate] = useState((new Date()).toString());  
   
+  useEffect(function(){
+    console.log('%cfunc => useEffect (componentDidMount) '+(++funcId), funcStyle);
+    document.title = number;
+    return function(){
+      console.log('%cfunc => useEffect return (componentWillUnMount) '+(++funcId), funcStyle);
+    }
+  }, []);
+  
   //side effect
   useEffect(function(){
-    console.log('%cfunc => useEffect (componentDidMount & componentDidUpdate) '+(++funcId), funcStyle);
-    document.title = number + ' : ' + _date;
+    console.log('%cfunc => useEffect number (componentDidMount & componentDidUpdate) '+(++funcId), funcStyle);
+    document.title = number;
     return function(){
-      console.log('%cfunc => useEffect return (componentDidMount & componentDidUpdate) '+(++funcId), funcStyle);
+      console.log('%cfunc => useEffect number return (componentDidMount & componentDidUpdate) '+(++funcId), funcStyle);
     }
-  });
+  }, [number]);
+
+  useEffect(function(){
+    console.log('%cfunc => useEffect _date (componentDidMount & componentDidUpdate) '+(++funcId), funcStyle);
+    document.title = _date;
+    return function(){
+      console.log('%cfunc => useEffect _date return (componentDidMount & componentDidUpdate) '+(++funcId), funcStyle);
+    }
+  }, [_date]);
 
   console.log('%cfunc => render '+(++funcId), funcStyle);
   return (
