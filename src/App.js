c1095d94e1961ba9f4605ab7c8bc93b841d58517
egoing <egoing@gmail.com>
2020-02-11 18:57:27 +0900

    4.1. 클래스에서 life cycle 구현하기

--- src/App.js
+++ src/App.js
@@ -36,13 +36,30 @@ function FuncComp(props){
     </div>
   );
 }
-
+var classStyle = 'color:red';
 class ClassComp extends React.Component{
   state = {
     number:this.props.initNumber,
     date:(new Date()).toString()
   }
+  componentWillMount(){
+    console.log('%cclass => componentWillMount', classStyle);
+  }
+  componentDidMount(){
+    console.log('%cclass => componentDidMount', classStyle);
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('%cclass => shouldComponentUpdate', classStyle);
+    return true;
+  }
+  componentWillUpdate(nextProps, nextState){
+    console.log('%cclass => componentWillUpdate', classStyle);
+  }
+  componentDidUpdate(nextProps, nextState){
+    console.log('%cclass => componentDidUpdate', classStyle);
+  }
   render(){
+    console.log('%cclass => render', classStyle);
     return (
       <div className="container">
         <h2>class style component</h2>
