a7da7b8230e49eff41e3b5c875846963e152e4de
egoing <egoing@gmail.com>
2020-02-11 13:29:14 +0900

    3.2. 함수에서 state 사용하기 (hook)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
@@ -12,26 +12,50 @@ function App() {
 }
 
 function FuncComp(props){
+  var numberState = useState(props.initNumber);
+  var number = numberState[0];
+  var setNumber = numberState[1];
+
+//   var dateState = useState((new Date()).toString());
+//   var _date = dateState[0];
+//   var setDate = dateState[1];
+
+  var [_date, setDate] = useState((new Date()).toString());  
+  
+  console.log('numberState', numberState);
   return (
     <div className="container">
       <h2>function style component</h2>
-      <p>Number : {props.initNumber}</p>
+      <p>Number : {number}</p>
+      <p>Date : {_date}</p>
+      <input type="button" value="random" onClick={
+          function(){
+            setDate((new Date()).toString());
+          }
+        }></input>
     </div>
   );
 }
 
 class ClassComp extends React.Component{
   state = {
-    number:this.props.initNumber
+    number:this.props.initNumber,
+    date:(new Date()).toString()
   }
   render(){
     return (
       <div className="container">
         <h2>class style component</h2>
         <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
         <input type="button" value="random" onClick={
           function(){
             this.setState({number:Math.random()})
+          }.bind(this)
+        }></input>
+        <input type="button" value="date" onClick={
+          function(){
+            this.setState({date:(new Date()).toString()})
           }.bind(this)
         }></input>
       </div>
